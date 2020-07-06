2b2ba90e5c6217e402b2bd19b9f57d8c09a173d8
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-05 21:00:22 -0600

    Add annotations to user model

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -1,8 +1,9 @@
 'use strict'
 
-// User
+// User -- this is similar to models in rails
 module.exports = function(sequelize, DataTypes) {
   let User = sequelize.define('users', {
+    // This is defining all the attributes that a user has
     name: {
       type: DataTypes.STRING
     },
@@ -16,10 +17,14 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT
     }
   })
-
+  // This is where relationships are defined for the model to be able to get the products, we'll have to build out relationships here
   User.associate = function(models) {
+    // this appears to set up a one to many relationship
     User.hasMany(models.Subscription)
   }
 
   return User
 }
+
+/* For this file we will have to add a couple more attributes to the user model including a description and a url image 
+*/
