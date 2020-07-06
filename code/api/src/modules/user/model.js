42e5f16413d8afdcad88b76dfaddb1fb947960b5
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-06 14:25:39 -0600

    Add image to user model

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -17,6 +17,10 @@ module.exports = function(sequelize, DataTypes) {
     },
     description: {
       type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=939&q=80"
     }
   })
 
