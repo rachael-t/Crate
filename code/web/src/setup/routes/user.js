b31502944f1583481da5e6ef94bd7709ca68cc8f
John Watterson <john@breakthrewbrew.com>
2020-07-06 11:55:34 -0600

    Annotation - explaining more of what is happening in user / routes to render specific components

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -6,6 +6,9 @@ import Subscriptions from '../../modules/user/Subscriptions'
 
 // User routes
 
+//the user routes shown below are responsible for rendering all of the Components, based on
+//the relative path of the user as seen in path:'/users/login' & component:'Login'
+
 // THESE routes: will need to be added to so that the route reflects the user's
 // actions for example edit: '/users/edit' — where the user can edit their profile
 // or products: '/users/products' — where the user can see all of their kept or returned products
