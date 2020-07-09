4806955106b2f4ffccd8402f87e17d668ebb23bc
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-07-09 15:40:58 -0600

    Create shipment model

---
+++ code/api/src/modules/shipments/model.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Subscription
+module.exports = function(sequelize, DataTypes) {
+  let Shipment = sequelize.define('shipments', {
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    crateId: {
+      type: DataTypes.INTEGER
+    },
+    deliveryDate: {
+      type: DataTypes.DATE
+    }
+  })
+
+  Shipment.associate = function(models) {
+    Shipment.belongsTo(models.User)
+    Shipment.belongsTo(models.Crate)
+  }
+
+  return Shipment
+}
