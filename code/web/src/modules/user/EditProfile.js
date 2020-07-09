f07f1dabafa5e3478c959f0396e94767d2a367bd
Rachael Thomas <rnthomas001@gmail.com>
2020-07-08 18:38:13 -0600

    :crystal-ball: Add functionality for uploading an image

--- code/web/src/modules/user/EditProfile.js
+++ code/web/src/modules/user/EditProfile.js
@@ -1,6 +1,6 @@
 // Imports
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
 import { Link } from 'react-router-dom'
@@ -9,14 +9,14 @@ import { Link } from 'react-router-dom'
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import Button from '../../ui/button'
-import { grey, grey2 } from '../../ui/common/colors'
+import { grey } from '../../ui/common/colors'
 import ImageTile from '../../ui/image/Tile'
 import { level1 } from '../../ui/common/shadows'
 
 // App Imports
 import userRoutes from '../../setup/routes/user'
 import { APP_URL } from '../../setup/config/env'
-
+import { upload, messageShow, messageHide } from '../common/api/actions.js'
 //Instead of importing Logout, we are going to import our action creator for updating user profile
 import { logout } from './api/actions'
 
@@ -25,11 +25,38 @@ class EditProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      image: '',
-      description: '',
-      email: '',
-      address: ''
+      user: {
+        image: '',
+        description: '',
+        email: '',
+        address: ''
+      }
     }
+  }
+
+  onUpload = (e) => {
+
+    let data = new FormData()
+    data.append('file', e.target.files[0])
+
+    this.props.upload(data)
+      .then(response => {
+        if (response.status === 200) {
+          // this.props.messageShow('File uploaded successfully.')
+
+          let user = {...this.state.user}
+          user.image = `/images/uploads/${ response.data.file }`
+
+          this.setState({
+            user
+          })
+        } else {
+          // this.props.messageShow('Please try again.')
+        }
+      })
+      // Need to add in messageShow for error once that's figured out
+      .catch(error => console.log(error))
+      
   }
 
   render() {
@@ -49,22 +76,20 @@ class EditProfile extends Component {
 
         <Grid>
           <GridCell justifyCenter={true} style={{ padding: '2em', textAlign: 'center'}}>
-
+            {/* look at CreateOrEdit line 318 to update this image */}
             <ImageTile width={300} height={530} shadow={level1} image={`${ APP_URL }/images/stock/men/1.jpg`} />
-            <form>
-              <input type="file"></input>
-              <Button theme="secondary" style={{ marginBottom: '4em', marginTop: '4em' }}>Upload Profile Image</Button>
-            </form>
 
             <H4 style={{ marginBottom: '0.5em' }}>{this.props.user.details.name}</H4>
 
-            <form>
-              <input value={this.props.user.details.email}></input>
-            </form>
+            <form onSubmit={this.onSubmit}>
+              <input type="file" onChange={this.onUpload}></input>
 
-            <Link to={userRoutes.profile.path}>
-              <Button theme="primary" style={{ marginLeft: '1em' }}>Save</Button>
-            </Link>
+              <input value={this.props.user.details.email}></input>
+
+              <Link to={userRoutes.profile.path}>
+                <Button type="submit" theme="primary" style={{ marginLeft: '1em' }}>Save</Button>
+              </Link>
+            </form>
 
           </GridCell>
         </Grid>
@@ -80,10 +105,12 @@ class EditProfile extends Component {
 // }
 
 // Component State
+// This function will need to be renamed
 function profileState(state) {
   return {
     user: state.user
   }
 }
 
-export default connect(profileState, { logout })(EditProfile)
+// Update the function name and any action creators that are imported/used
+export default connect(profileState, { upload })(EditProfile)
