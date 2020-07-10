4c61385a5711ec4f74f3e9b5b9566c30e6e83f95
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-07-10 13:48:49 -0600

    :crystal_ball: Adding input fields for user to modify shipping address

--- code/web/src/modules/user/EditProfile.js
+++ code/web/src/modules/user/EditProfile.js
@@ -13,12 +13,14 @@ import Button from '../../ui/button'
 import { grey } from '../../ui/common/colors'
 import ImageTile from '../../ui/image/Tile'
 import { level1 } from '../../ui/common/shadows'
+import { Input, Textarea, Select } from '../../ui/input'
 
 // App Imports
 import userRoutes from '../../setup/routes/user'
 import { routeImage } from '../../setup/routes'
 import { upload, messageShow, messageHide } from '../common/api/actions.js'
 import { updateUserProfile } from './api/actions'
+import { slug } from '../../setup/helpers'
 
 // Component
 class EditProfile extends Component {
@@ -31,6 +33,9 @@ class EditProfile extends Component {
         description: this.props.user.details.description,
         email: this.props.user.details.email,
         address: this.props.user.details.address,
+        city: this.props.user.details.city,
+        state: this.props.user.details.state,
+        zip: this.props.user.details.zip,
         id: this.props.user.details.id
       }
     }
@@ -71,7 +76,17 @@ class EditProfile extends Component {
       
   }
 
+  onChange = (e) => {
+    let user = this.state.user
+    user[e.target.name] = e.target.value
+
+    this.setState({
+      user
+    })
+  }
+
   render() {
+  
     return(
       <div>
         {/* SEO */}
@@ -94,10 +109,64 @@ class EditProfile extends Component {
 
             <form>
               <input type="file" onChange={this.onUpload}></input>
-
-              <input value={this.props.user.details.email}></input>
-
-                <Button onClick={this.onSubmit} theme="primary" style={{ marginLeft: '1em' }}>Save</Button>
+              <Input
+                    type="text"
+                    placeholder="email"
+                    required="required"
+                    name="email"
+                    autoComplete="off"
+                    value={this.state.user.email}
+                    onChange={this.onChange}
+                  />
+              <label for="description">Enter your User Description:</label>
+              <Textarea
+                    fullWidth={true}
+                    placeholder="Description"
+                    required="required"
+                    name="description"
+                    value={this.state.user.description}
+                    onChange={this.onChange}
+                    style={{ marginTop: '1em' }}
+                  />
+              <label for="address">Enter your Shipping Address:</label>
+              <Input
+                    type="text"
+                    placeholder="Street Address"
+                    required="required"
+                    name="address"
+                    value={this.state.user.address}
+                    onChange={this.onChange}
+                    style={{ marginTop: '1em' }}
+                  />
+              <Input
+                    type="text"
+                    placeholder="City"
+                    required="required"
+                    name="city"
+                    value={this.state.user.city}
+                    onChange={this.onChange}
+                    style={{ marginTop: '1em' }}
+                  />
+              <Input
+                    type="text"
+                    placeholder="State"
+                    required="required"
+                    name="state"
+                    value={this.state.user.state}
+                    onChange={this.onChange}
+                    style={{ marginTop: '1em' }}
+                  />
+              <Input
+                    type="text"
+                    placeholder="Zip Code"
+                    required="required"
+                    name="zip"
+                    value={this.state.user.zip}
+                    onChange={this.onChange}
+                    style={{ marginTop: '1em' }}
+                  />
+                
+              <Button onClick={this.onSubmit} theme="primary" style={{ marginLeft: '1em' }}>Save</Button>
             </form>
 
           </GridCell>
