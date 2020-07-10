00e291ddd72c04048fe8ea559abc6579a6d75d17
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-07-09 20:01:22 -0600

    Add hook to subscription

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -9,7 +9,14 @@ module.exports = function(sequelize, DataTypes) {
     crateId: {
       type: DataTypes.INTEGER
     }
-  })
+  }, {
+      hooks: {
+        beforeUpdate: function() {
+          console.log('this beforeUdpate hook worked');
+        }
+      }
+    }
+  );
 
   Subscription.associate = function(models) {
     Subscription.belongsTo(models.User)
