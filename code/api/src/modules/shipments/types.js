c902f1f9650ff7f4b59ae668c1507d40df377b84
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-11 16:46:54 -0600

    Add products type to shipment type

--- code/api/src/modules/shipments/types.js
+++ code/api/src/modules/shipments/types.js
@@ -1,9 +1,10 @@
 // Imports
-import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql'
+import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } from 'graphql'
 
 // App Imports
 import { UserType } from '../user/types'
 import SubscriptionType from '../subscription/types'
+import { ProductType} from '../product/types'
 
 // Shipment type
 const ShipmentType = new GraphQLObjectType({
@@ -15,6 +16,7 @@ const ShipmentType = new GraphQLObjectType({
     user: { type: UserType },
     subscription: { type: SubscriptionType },
     deliveryDate: { type: GraphQLString},
+    products: { type: new GraphQLList(ProductType) },
     createdAt: { type: GraphQLString },
     updatedAt: { type: GraphQLString }
   })
